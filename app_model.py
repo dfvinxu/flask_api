a5851741779930615b44931cebaa03b411c54028
dfvinxu <df.vinxu@gmail.com>
2022-05-04 13:10:07 +0200

    first commit

---
+++ app_model.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import os
+import pickle
+
+os.chdir(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+model = pickle.load(open('ad_model','rb'))
+
+@app.route('/api/v1/predict', methods=['GET'])
+def predict():
+    
+    tv = request.args.get('tv', None)
+    radio = request.args.get('radio', None)
+    newspaper = request.args.get('newspaper', None)
+
+    if tv is None or radio is None or newspaper is None:
+        return "Args empty, the data are not enough to predict"
+    else:
+        prediction = model.predict([[tv,radio,newspaper]])
+    
+    return jsonify({'predictions': prediction[0]})
+
+app.run()
