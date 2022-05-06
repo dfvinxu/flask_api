f18121e9ed72b7e37e65342130d7b488579a34c5
dfvinxu <df.vinxu@gmail.com>
2022-05-06 09:29:28 +0200

    union train y app

--- app_model.py
+++ app_model.py
@@ -1,4 +1,8 @@
 from flask import Flask, jsonify, request
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import Lasso
+import pandas as pd
 import os
 import pickle
 
@@ -22,5 +26,28 @@ def predict():
         prediction = model.predict([[tv,radio,newspaper]])
     
     return jsonify({'predictions': prediction[0]})
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+data = pd.read_csv('/home/vinxu/flask_api/data/Advertising.csv', index_col=0)
+
+@app.route('/api/v1/retrain', methods=['GET'])
+def retrain():
+
+    X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales']),
+                                                    data['sales'],
+                                                    test_size = 0.20,
+                                                    random_state=42)
+
+    model = Lasso(alpha=6000)
+    model.fit(X_train, y_train)
+
+    pickle.dump(model, open('/home/vinxu/flask_api/ad_model.pkl', 'wb'))
+
+    mse = mean_squared_error(y_test, model.predict(X_test))
+    rmse = np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
+
+    return {'MSE':mse,'RMSE':rmse}
 
 app.run()
