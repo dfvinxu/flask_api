8312ffee130acd1a60ed3275623cb7d7b1172e76
dfvinxu <df.vinxu@gmail.com>
2022-05-06 09:44:14 +0200

    last update

--- app_model.py
+++ app_model.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import Lasso
 import pandas as pd
+import numpy as np
 import os
 import pickle
 
@@ -11,10 +12,14 @@ os.chdir(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-model = pickle.load(open('/home/vinxu/flask_api/ad_model.pkl','rb'))
+@app.route('/', methods=['GET'])
+def welcome():
+    return 'Welcome to my advertising API.'
 
 @app.route('/api/v1/predict', methods=['GET'])
 def predict():
+
+    model = pickle.load(open('/home/vinxu/flask_api/ad_model.pkl','rb'))
     
     tv = request.args.get('tv', None)
     radio = request.args.get('radio', None)
@@ -32,9 +37,11 @@ app.config['DEBUG'] = True
 
 data = pd.read_csv('/home/vinxu/flask_api/data/Advertising.csv', index_col=0)
 
-@app.route('/api/v1/retrain', methods=['GET'])
+@app.route('/api/v1/retrain', methods=['PUT'])
 def retrain():
 
+    data = pd.read_csv('/home/vinxu/flask_api/data/Advertising.csv', index_col=0)
+    
     X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales']),
                                                     data['sales'],
                                                     test_size = 0.20,
